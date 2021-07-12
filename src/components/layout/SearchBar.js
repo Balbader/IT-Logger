d31a8f97b94464532c92557c2816a89106a884dc
Balbader <15971200+Balbader@users.noreply.github.com>
2021-07-12 15:18:12 +0200

    removed required from input label in return statement as it generated a logs err in Logs component

--- src/components/layout/SearchBar.js
+++ src/components/layout/SearchBar.js
@@ -6,7 +6,7 @@ const SearchBar = () => {
 			<div className='nav-wrapper'>
 				<form>
 					<div className="input-field">
-						<input type="search" id="search" required />
+						<input type="search" id="search" />
 						<label htmlFor="search" className="label-icon">
 							<i className="material-icons">search</i>
 						</label>
