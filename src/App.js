313d35478ce6907b566949bbcb032860fe5a1b21
Balbader <15971200+Balbader@users.noreply.github.com>
2021-07-12 13:44:27 +0200

    cleanUp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
+
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
-
 import './App.css';
 
 const App = () => {
