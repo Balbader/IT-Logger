d6461648e49fb525ff4f5a20d1edb9455f9e6899
Balbader <15971200+Balbader@users.noreply.github.com>
2021-07-13 18:51:09 +0200

    search server logs function added

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -6,7 +6,8 @@ import {
 	DELETE_LOG,
 	SET_CURRENT,
 	CLEAR_CURRENT,
-	UPDATE_LOG
+	UPDATE_LOG,
+	SEARCH_LOGS
 } from './types';
 
 // Get logs from server
@@ -102,6 +103,27 @@ export const updateLog = log => async dispatch => {
 		});
 	}
 }
+
+// Search server logs
+export const searchLogs = text => async dispatch => {
+	try {
+		setLoading();
+
+		const res = await fetch(`/logs?q=${text}`);
+		const data = await res.json();
+
+		dispatch({
+			type: SEARCH_LOGS,
+			payload: data
+		});
+	} catch (err) {
+		dispatch({
+			type: LOGS_ERROR,
+			payload: err.response.data
+		});
+	}
+}
+
 // Set current log
 export const setCurrent = log => {
 	return {
